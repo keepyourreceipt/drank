a0413c1fa2b0c4a18fabb7d9e8e5169990c195d1
Michael Peterman <michaelkpeterman@gmail.com>
2017-06-16 16:32:07 -0400

    Add master table view controller and configure cell(s)

---
+++ Drank/MasterTableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MasterTableViewController.swift
+//  Drank
+//
+//  Created by Steve Gabriel on 6/16/17.
+//  Copyright © 2017 Michael K Peterman. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MasterTableViewCell", for: indexPath)
+        
+        let cellLabel = cell.viewWithTag(1) as! UILabel
+        cellLabel.text = "Something"
+       
+
+        return cell
+    }
+    
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
