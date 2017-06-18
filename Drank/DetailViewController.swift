c61ead1805b90d7521d0e0f77c4d96c8bee653e6
Michael <michaelkpeterman@gmail.com>
2017-06-17 22:22:26 -0400

    Set height of detail view description to dynamic value

--- Drank/DetailViewController.swift
+++ Drank/DetailViewController.swift
@@ -30,6 +30,7 @@ class DetailViewController: UIViewController {
         recipeDirections.numberOfLines = 0
         
         recipeDirections.text = recipeDirectionsText
+        
     }
 
 
--- Drank/RecipeProvider.swift
+++ Drank/RecipeProvider.swift
@@ -17,5 +17,5 @@ struct Recipe {
 
 let recipies = [
     Recipe(title: "Rum and Coke", imageName: "rumandcoke", ingredients: ["Rum", "Coke"], directions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
-    Recipe(title: "Rum Runner", imageName: "rumrunner", ingredients: ["Rum", "Juice", "Other stuff"], directions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    Recipe(title: "Rum Runner", imageName: "rumrunner", ingredients: ["Rum", "Juice", "Other stuff"], directions: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.")
 ]
